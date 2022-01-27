d074302654b68e22a26ed156952c1c83bbdec2bc
MaxenceLvl <33911862+MaxenceLvl@users.noreply.github.com>
2022-01-27 11:06:45 +0100

    Add data for the show in Profile

---
+++ app/src/data/beers.js
@@ -0,0 +1,122 @@
+const beers = [
+  {
+    profile_image:
+      "https://res.cloudinary.com/dzt4ytngw/image/upload/v1612455810/udta51g4jezekwmjlmr2.png",
+    typeFamily: "Ale (belgisch)",
+    beerType: "Wit Bier",
+    id: "601c1ed679ebde001c697611",
+    brewery: {
+      street: "Rijksweg 33",
+      gallery: [
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1595503050/ybntdbaxzza1odrulkw9",
+      ],
+      city: "Bavikhove ",
+      region: "West-Vlaanderen",
+      name: "De Brabandere Brouwerij",
+      id: "59248a688b6d466eaca5e036",
+      profile_image:
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1595503050/m8x8n5ozxccaz9ycrcfm.png",
+      homepage: "www.brouwerijdebrabandere.be",
+      description:
+        "The love of brewing is in the genes of the De Brabandere family. With the best brewers at their side, tradition continues for generation and builds on the rich selection of beers. We have shared this passion with all our employees for years: they literally bring &quot;life to the brewery&quot;. And live in your glass, at home or with friends. Because our passion gives pleasant and sparkling moments with friends a taste.",
+      master_brewer: "",
+      country: "BEL",
+      sinceYear: 1894,
+    },
+    fermentation: "bottom fermented",
+    display_name: "De Brabandere Wittekerke Wild",
+    alcohol: 500,
+    IBU: null,
+    description: "",
+  },
+  {
+    fermentation: "bottom fermented",
+    brewery: {
+      homepage: "www.arcobraeu.de",
+      profile_image:
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1544214738/lpr1evgmnahpisq6ucsy.jpg",
+      description:
+        "Arcobräu is a brewery in the Lower Bavarian town of Moos. It is owned by the Arco-Zinneberg family. With a production of about 140,000 hectoliters of beer a year, the brewery is one of the larger breweries in Lower Bavaria. The former Schlossbrauerei Moos was first documented in 1567, but it was not until the merger of the various family-owned breweries in 1960 that today&#39;s company Arcobräu with its headquarters in Moos was built. In the early 1990s, Arcobräu took over a number of smaller breweries in Eastern Bavaria. Since 1974, Arcobräu hosts its own folk festival every Whitsun, widely known as the Mooser Pentecost in the region. In addition, Arcobräu since 1922 at the second largest Bavarian folk festival, the Gäubodenvolksfest in Straubing represented. At the end of 2010, a dispute between Arcobräu and the city of Deggendorf began, which had ignited the question of an additional marquee at the Deggendorfer Volksfest and culminated in allegations of corruption against local elected officials. This dispute, referred to by the regional media as a &quot;beer war&quot;, continued in the first half of 2011 and also found supraregional attention. In March 2016, the Arcobräu took over the castle brewery Irlbach.",
+      id: "584ed298dcd720d123e6e26d",
+      name: "Arcobräu Gräfliches Brauhaus",
+      master_brewer: "Günther Breitenfellner",
+      street: "Schlossalee 1",
+      city: "Moos",
+      region: "Bayern",
+      sinceYear: 1567,
+      country: "GER",
+      gallery: [
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1544214738/oocblsx0dtyi2cyaplox.jpg",
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1544214738/yvnvnaftj36kquvf7pp0.jpg",
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1544214738/jpxxss5uxidxwq8w2svm.jpg",
+      ],
+    },
+    alcohol: 720,
+    profile_image:
+      "https://res.cloudinary.com/dzt4ytngw/image/upload/v1519312145/wkvm163v2kk4k6wqalf8.png",
+    id: "5a8edcf295557e88ac676d02",
+    display_name: "Arcobräu Coronator",
+    description:
+      "Extra brewed with more than 18 percent original wort. An old Bavarian dark double buck. Malted and tasty.",
+    typeFamily: "Bock",
+    IBU: 25,
+    beerType: "Dunkler Doppelbock",
+  },
+  {
+    id: "59554a8e510f6880adfe9fee",
+    fermentation: "obergärig",
+    profile_image:
+      "https://res.cloudinary.com/dzt4ytngw/image/upload/v1498762098/xzv62ro2fov5junz2xt8.png",
+    description:
+      "Full-bodied, intense and a touch of caramel. Together with the spicy, citrus-like bitterness, they give this beer a very fresh, full and bitter aftertaste. Specialty beer created to celebrate the twenty-fifth anniversary of Delirium Tremens.",
+    beerType: "Strong Ale",
+    alcohol: 700,
+    IBU: 55,
+    typeFamily: "Ale (belgisch)",
+    brewery: {
+      homepage: "www.delirium.be",
+      region: "Oost-Vlaanderen",
+      gallery: [],
+      description:
+        "Huyghe Brewery (Anno 1654) is a family brewery in Melle (near Ghent), best known for its Delirium Tremens beer, which is sold all over the world and recognizable by its pink elephant. In addition to the delirium beers, the &quot;Brouwerij Huyghe&quot; also brews the abbey animal Averbode. Based on our wheat beer, we also brew the fair trade Mongozo beers. All these beers are brewed with passion and tradition in a modern brewery. With constant innovation, we can regularly launch new beers of the highest quality on the market.",
+      sinceYear: 1654,
+      city: "Melle",
+      name: "Brouwerij Huyghe",
+      id: "59554a3f510f6880adfe9fec",
+      country: "BEL",
+      street: "Geraardsbergsesteenweg 14 B",
+      profile_image:
+        "https://res.cloudinary.com/dzt4ytngw/image/upload/v1564492946/bzz0rnbzzsv6syovjsvm.jpg",
+    },
+    display_name: "Delirium Argentum",
+  },
+  {
+    alcohol: 750,
+    id: "5cf14510368f98133b67aabf",
+    beerType: "IPA",
+    brewery: {
+      country: "USA",
+      sinceYear: 1870,
+      street: "One Busch Place.",
+      region: "Missouri",
+      master_brewer: "Eberhard Anheuser",
+      gallery: [],
+      city: "St. Louis",
+      name: "Anheuser-Busch Companies",
+      homepage: "www.anheuser-busch.com/",
+      description:
+        "Anheuser-Busch is an American brewery headquartered in St. Louis, Missouri. Since November 2008, she is part of the Anheuser-Busch-InBev Group, based in Leuven, Belgium. The best known brand is Budweiser (Bud), which has been brewed since 1876 and distributed worldwide in more than 70 countries. In 2007, a brewing volume of 190 million hectoliters was achieved",
+      id: "588dd526f03a75106a35d02b",
+    },
+    fermentation: "obergärig",
+    typeFamily: "India Pale Ale",
+    display_name: "Leffe Royale IPA",
+    description:
+      "Strong dark orange color with creamy white head. The aroma is light and fruity with a fine bitterness and a dry fruity finish.",
+    profile_image:
+      "https://res.cloudinary.com/dzt4ytngw/image/upload/v1559315764/ljcahqwkq0vztkpm7j9y.png",
+    IBU: 0,
+  },
+];
+
+export default beers;
--- app/src/screens/Profile.js
+++ app/src/screens/Profile.js
@@ -3,36 +3,62 @@ import React, { useContext, useEffect, useState } from "react";
 import {
   Text,
   View,
-  TextInput,
   Keyboard,
   Alert,
   Image,
   ActivityIndicator,
   ImageBackground,
+  ScrollView,
 } from "react-native";
 import { Card } from "react-native-paper";
 import Button from "../components/Button";
 import { UserContext } from "../data/UserContext";
+import BeerCard from "../components/BeerCard";
 import screensStyles from "./Styles";
-import Constants from "expo-constants";
+import { getBeerBy } from "../data/api";
+import beers from "../data/beers";
 
 const inputStyle = [screensStyles.input2, screensStyles.margin];
 
 const Profile = () => {
-  const { user, logout, modifyUser } = useContext(UserContext);
+  const { user, userid, logout, modifyUser } = useContext(UserContext);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [editing, setEditing] = useState(false);
-  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  // const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
 
-  useEffect(() => {
-    Keyboard.addListener("keyboardDidShow", () => setIsKeyboardOpen(true));
-    Keyboard.addListener("keyboardDidHide", () => setIsKeyboardOpen(false));
-    return () => {
-      Keyboard.removeAllListeners("keyboardDidShow");
-      Keyboard.removeAllListeners("keyboardDidHide");
-    };
-  }, []);
+  // useEffect(() => {
+  //   Keyboard.addListener("keyboardDidShow", () => setIsKeyboardOpen(true));
+  //   Keyboard.addListener("keyboardDidHide", () => setIsKeyboardOpen(false));
+  //   return () => {
+  //     Keyboard.removeAllListeners("keyboardDidShow");
+  //     Keyboard.removeAllListeners("keyboardDidHide");
+  //   };
+  // }, []);
+
+  const beerIds = [];
+
+  if (userid != null && user != null) {
+    for (let index = 0; index < user.beers.length; index++) {
+      const element = user.beers[index]._path.segments[1];
+      beerIds.push(element);
+    }
+  }
+
+  console.log(beers);
+
+  // const example = async (beersIds) => {
+  //   if (beersIds) {
+  //     for (const b of beersIds) {
+  //       console.log(b);
+  //       const res = await getBeerBy(b);
+  //       console.log(res);
+  //     }
+  //   }
+  // };
+
+  // example(beerIds);
+  // console.log(beers);
 
   const validate = () => {
     if (firstName.length === 0) {
@@ -58,29 +84,30 @@ const Profile = () => {
 
   return (
     <View style={[screensStyles.containerDetail]}>
-      <ImageBackground
-        source={require("../img/beer_background.png")}
-        style={[screensStyles.image]}
-      >
-        <Card style={{ borderWidth: 2, borderRadius: 14 }}>
-          <View>
-            {editing ? (
-              <UploadButton />
-            ) : (
-              <View>
-                {user.avatar ? (
-                  <Image source={{ uri: user.avatar }} />
-                ) : (
-                  <Ionicons name="person" color="#FFFFFF" size={32} />
-                )}
+      <ScrollView>
+        <ImageBackground
+          source={require("../img/beer_background.png")}
+          style={[screensStyles.image]}
+        >
+          <Card style={{ borderWidth: 2, borderRadius: 14 }}>
+            <View>
+              {editing ? (
+                <UploadButton />
+              ) : (
+                <View>
+                  {user.avatar ? (
+                    <Image source={{ uri: user.avatar }} />
+                  ) : (
+                    <Ionicons name="person" color="#FFFFFF" size={32} />
+                  )}
+                </View>
+              )}
+              <View style={{ alignItems: "stretch" }}>
+                <Text style={[inputStyle]}>
+                  Bonjour {user.firstName} {user.lastName} !
+                </Text>
               </View>
-            )}
-            <View style={{ alignItems: "stretch" }}>
-              <Text style={[inputStyle]}>
-                Bonjour {user.firstName} {user.lastName} !
-              </Text>
-            </View>
-            {/* <View style={[{ alignItems: "stretch" }]}>
+              {/* <View style={[{ alignItems: "stretch" }]}>
             {editing ? (
               <TextInput
                 value={lastName}
@@ -91,19 +118,36 @@ const Profile = () => {
               <Text style={[inputStyle]}>{user.lastName} !</Text>
             )}
           </View> */}
-            <Button
+              {/* <Button
               title={editing ? "Valider" : "Editer mon profil"}
               style={[screensStyles.button]}
               onPress={editing ? validate : () => setEditing(true)}
-            />
-            <Button
-              title="Me déconnecter"
-              style={[screensStyles.button]}
-              onPress={logout}
-            />
-          </View>
-        </Card>
-      </ImageBackground>
+            /> */}
+              <Button
+                title="Me déconnecter"
+                style={[screensStyles.button]}
+                onPress={logout}
+              />
+            </View>
+          </Card>
+          <Card style={{ marginTop: 10 }}>
+            <View>
+              {user != null ? (
+                beers.map((b) => (
+                  <BeerCard
+                    beer={b}
+                    onPress={() => {
+                      props.navigation.navigate("BeerDetails", { beer: b });
+                    }}
+                  />
+                ))
+              ) : (
+                <Text>No favorite beer add one ! </Text>
+              )}
+            </View>
+          </Card>
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 };
